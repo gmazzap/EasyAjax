4a5798751a1a39243eab85d74016e30ff2bd38f3
Giuseppe Mazzapica <giuseppe@zoom-multiservice.it>
2013-10-28 05:29:43 +0100

    Add doc block to easyajax func

--- EasyAjax.php
+++ EasyAjax.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: Easy Ajax
  * Author: Giuseppe Mazzapica
@@ -15,6 +14,15 @@ if ( ! defined( 'ABSPATH' ) ) die();
 if ( ! function_exists( 'easyajax' ) ) {
 
 
+    /**
+     * Setup and launch easyajax
+     *
+     * @param object $scope the object scope that will execute action if given
+     * @param string $where 'priv', 'nopriv' or 'both'. Limit actions to logged in users or not
+     * @param array $allowed list of allowed actions
+     * @return null
+     * @access public
+     */
     function easyajax( $scope = '', $where = '', $allowed = array() ) {
         $action = ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ? 'admin_init' : 'wp_footer';
         if ( did_action( $action ) ) return;
