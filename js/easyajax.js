3071dcef8c7b18d1c73f89ad61456ee2c6d3caed
Giuseppe Mazzapica <giuseppe@zoom-multiservice.it>
2013-10-28 05:40:20 +0100

    v 0.3.0
    folders, rearrage, use composer, js minify, refactoring

--- js/easyajax.js
+++ js/easyajax.js
@@ -1,8 +1,1 @@
-jQuery(document).ready(function($) {
-	
-	$.ajaxSetup({
-		type: "POST",
-		url: easy_ajax_vars.ajaxurl
-	});
-
-});
+jQuery(document).ready(function($){$.ajaxSetup({type:"POST",url:easy_ajax_vars.ajaxurl})});
